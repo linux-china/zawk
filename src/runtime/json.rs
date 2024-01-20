5334f128bdb5a5500cce5541d8d100ad1dbe3051
linux_china <libing.chen@gmail.com>
2024-01-20 21:39:31 +0800

    feat: add from_json()

--- src/runtime/json.rs
+++ src/runtime/json.rs
@@ -1,29 +1,39 @@
 use std::collections::HashMap;
 use miniserde::json;
 use miniserde::json::{Number, Value};
-use crate::runtime::{Str, StrMap};
+use crate::runtime::{SharedMap, Str, StrMap};
 
 pub fn to_json(obj: &StrMap<Str>) -> String {
-    let mut json_obj: HashMap<String,Value> = HashMap::new();
-    obj.iter(|map | {
-        for (key, value)  in map {
+    let mut json_obj: HashMap<String, Value> = HashMap::new();
+    obj.iter(|map| {
+        for (key, value) in map {
             if !value.is_empty() {
                 let value_text = value.to_string();
                 if value_text.contains('.') { // check float
                     if let Ok(num) = value_text.parse::<f64>() {
                         json_obj.insert(key.to_string(), Value::Number(Number::F64(num)));
                     } else {
-                        json_obj.insert(key.to_string(),Value::String(value_text));
+                        json_obj.insert(key.to_string(), Value::String(value_text));
                     }
                 } else { // check integer
                     if let Ok(num) = value_text.parse::<i64>() {
-                        json_obj.insert(key.to_string(),Value::Number(Number::I64(num)));
+                        json_obj.insert(key.to_string(), Value::Number(Number::I64(num)));
                     } else {
-                        json_obj.insert(key.to_string(),Value::String(value_text));
+                        json_obj.insert(key.to_string(), Value::String(value_text));
                     }
                 }
             }
         }
     });
     json::to_string(&json_obj)
+}
+
+pub fn from_json(json_text: &str) -> StrMap<Str> {
+    let mut map = hashbrown::HashMap::new();
+    if let Ok(json_obj) = json::from_str::<HashMap<String,String>>(json_text) {
+        for (key, value) in json_obj {
+            map.insert(Str::from(key), Str::from(value));
+        }
+    }
+    return StrMap::from(map);
 }
