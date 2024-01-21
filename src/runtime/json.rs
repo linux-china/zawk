8ca60609c74e4e5b893706adafe59978f9733f81
linux_china <libing.chen@gmail.com>
2024-01-21 17:47:35 +0800

    feat: add json array support for from_json()

--- src/runtime/json.rs
+++ src/runtime/json.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 use miniserde::json;
 use miniserde::json::{Number, Value};
 use crate::runtime::{Str, StrMap};
-use crate::runtime::str_escape::escape;
 
 pub fn to_json(obj: &StrMap<Str>) -> String {
     let mut json_obj: HashMap<String, Value> = HashMap::new();
@@ -30,6 +29,9 @@ pub fn to_json(obj: &StrMap<Str>) -> String {
 }
 
 pub fn from_json(json_text: &str) -> StrMap<Str> {
+    if json_text.starts_with('[') {
+        return from_json_array(json_text);
+    }
     let mut map = hashbrown::HashMap::new();
     if let Ok(json_obj) = json::from_str::<HashMap<String, Value>>(json_text) {
         for (key, value) in json_obj {
@@ -57,8 +59,44 @@ pub fn from_json(json_text: &str) -> StrMap<Str> {
             }
         }
     }
-    return StrMap::from(map);
+    StrMap::from(map)
 }
+
+fn from_json_array(json_text: &str) -> StrMap<Str> {
+    let mut map = hashbrown::HashMap::new();
+    let result = json::from_str::<Vec<Value>>(json_text);
+    if let Ok(json_array) = result {
+        for (index, json_value) in json_array.iter().enumerate() {
+            let key = index.to_string();
+            match json_value {
+                Value::Bool(b) => {
+                    if *b {
+                        map.insert(Str::from(key), Str::from("1"));
+                    } else {
+                        map.insert(Str::from(key), Str::from("0"));
+                    }
+                }
+                Value::Number(num) => {
+                    map.insert(Str::from(key), Str::from(num.to_string()));
+                }
+                Value::String(s) => {
+                    map.insert(Str::from(key), Str::from(s.to_string()));
+                }
+                Value::Array(arr) => {
+                    map.insert(Str::from(key), Str::from(json::to_string(&arr)));
+                }
+                Value::Object(obj) => {
+                    map.insert(Str::from(key), Str::from(json::to_string(&obj)));
+                }
+                Value::Null => {
+                    map.insert(Str::from(key), Str::from(json::to_string("")));
+                }
+            }
+        }
+    }
+    StrMap::from(map)
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -70,6 +108,16 @@ mod tests {
         let json_text = r#"  {"id": 1, "names" : ["first", "second"] } "#;
         let json_object = json::from_str::<HashMap<String, Value>>(json_text).unwrap();
         let text = json::to_string(&json_object);
+        println!("{}", text);
+    }
+
+    #[test]
+    fn test_array() {
+        //language=json
+        let json_text = r#"  [1, 2, "third"] "#;
+        let json_array = json::from_str::<Vec<Value>>(json_text).unwrap();
+        println!("size: {}", json_array.len());
+        let text = json::to_string(&json_array);
         println!("{}", text);
     }
 }
