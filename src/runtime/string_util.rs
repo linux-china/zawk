85c2e04911581ee5ad93d6f29ce50986776d835d
linux_china <libing.chen@gmail.com>
2024-02-21 15:38:55 +0800

    feat: add strcmp() function

--- src/runtime/string_util.rs
+++ src/runtime/string_util.rs
@@ -15,6 +15,16 @@ pub fn pad_both(text: &str, len: usize, pad: &str) -> String {
     text.pad(len, pad_char, Alignment::MiddleRight, false)
 }
 
+pub fn strcmp(text1: &str, text2: &str) -> i64 {
+    return if text1 == text2 {
+        0
+    } else if text1 < text2 {
+        -1
+    } else {
+        1
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,5 +33,12 @@ mod tests {
     fn test_pad_left() {
         let text = pad_both("hello", 100, "*");
         println!("{}", text);
+    }
+
+    #[test]
+    fn test_strcmp() {
+        let text1 = "hello";
+        let text2 = "Hello";
+        println!("{}", strcmp(text1, text2));
     }
 }
