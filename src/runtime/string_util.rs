14b3a6cbb842218d93032b92ca7b85358076d495
linux_china <libing.chen@gmail.com>
2024-02-26 14:22:10 +0800

    feat: add words() function

--- src/runtime/string_util.rs
+++ src/runtime/string_util.rs
@@ -1,4 +1,6 @@
 use pad::{Alignment, PadStr};
+use unicode_segmentation::UnicodeSegmentation;
+use crate::runtime::{IntMap, Str};
 
 pub fn pad_left(text: &str, len: usize, pad: &str) -> String {
     let pad_char = pad.chars().next().unwrap();
@@ -25,8 +27,19 @@ pub fn strcmp(text1: &str, text2: &str) -> i64 {
     };
 }
 
+pub(crate) fn words<'a>(text: &str) -> IntMap<Str<'a>> {
+    let result: IntMap<Str> = IntMap::default();
+    let mut index: i64 = 1;
+    for word in text.unicode_words() {
+        result.insert(index, Str::from(word.to_string()));
+        index = index + 1;
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
+    use unicode_segmentation::UnicodeSegmentation;
     use super::*;
 
     #[test]
@@ -40,5 +53,14 @@ mod tests {
         let text1 = "hello";
         let text2 = "Hello";
         println!("{}", strcmp(text1, text2));
+    }
+
+    #[test]
+    fn test_words() {
+        let text="Hello , world! could you give a 名称?";
+        let words = text.unicode_words();
+        for word in words {
+           println!("{}", word);
+        }
     }
 }
