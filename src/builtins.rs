5f30931ab2cf6652a4b48923929b3420b23f5f72
Libing Chen <libing.chen@gmail.com>
2024-04-27 15:56:04 +0800

    build: update to version 0.5.5

--- src/builtins.rs
+++ src/builtins.rs
@@ -10,7 +10,7 @@ use smallvec::smallvec;
 
 use std::convert::TryFrom;
 
-pub const VERSION: &'static str = "0.5.4";
+pub const VERSION: &'static str = "0.5.5";
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Function {
