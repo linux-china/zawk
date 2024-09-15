c4fe2292866bedff027e00bae76c82df3095a016
Libing Chen <libing.chen@gmail.com>
2024-09-15 22:10:38 +0800

    refactor: rollback to postgres for synchronous

--- src/runtime/postgres.rs
+++ src/runtime/postgres.rs
@@ -1,33 +1,27 @@
-use std::any::Any;
 use std::collections::HashMap;
 use std::sync::Mutex;
 use chrono::Utc;
 use lazy_static::lazy_static;
 use crate::runtime::{Int, IntMap, Str};
 use crate::runtime::csv::vec_to_csv;
+use postgres::{Client, NoTls};
 use uuid::Uuid;
-use sqlx::{Column, Executor, PgPool, Row, TypeInfo};
-use sqlx::postgres::{PgPoolOptions, PgRow};
 
 lazy_static! {
-    static ref PG_POOLS: Mutex<HashMap<String, PgPool>> = Mutex::new(HashMap::new());
+    static ref PG_POOLS: Mutex<HashMap<String, Client>> = Mutex::new(HashMap::new());
 }
 
-pub(crate) async fn pg_query<'a>(db_url: &str, sql: &str) -> IntMap<Str<'a>> {
+pub(crate) fn pg_query<'a>(db_url: &str, sql: &str) -> IntMap<Str<'a>> {
     let map: IntMap<Str> = IntMap::default();
     let mut pools = PG_POOLS.lock().unwrap();
-    if !pools.contains_key(db_url) {
-        let pool = PgPoolOptions::new()
-            .max_connections(1)
-            .connect(db_url).await.unwrap();
-        pools.insert(db_url.to_string(), pool);
-    }
-    let pool = pools.get(db_url).unwrap();
-    let rows =sqlx::query(sql).fetch_all(pool).await.unwrap();
+    let client = pools.entry(db_url.to_string()).or_insert_with(|| {
+        Client::connect(db_url, NoTls).unwrap()
+    });
+    let rows = client.query(sql, &[]).unwrap();
     let mut index = 1;
     for row in rows {
         let mut items: Vec<String> = vec![];
-        for i in 0..row.columns().len() {
+        for i in 0..row.len() {
             let text_value = reflective_get(&row, i);
             items.push(text_value);
         }
@@ -38,71 +32,64 @@ pub(crate) async fn pg_query<'a>(db_url: &str, sql: &str) -> IntMap<Str<'a>> {
     map
 }
 
-pub(crate) async fn pg_execute(db_url: &str, sql: &str) -> Int {
+pub(crate) fn pg_execute(db_url: &str, sql: &str) -> Int {
     let mut pools = PG_POOLS.lock().unwrap();
-    if !pools.contains_key(db_url) {
-        let pool = PgPoolOptions::new()
-            .max_connections(1)
-            .connect(db_url).await.unwrap();
-        pools.insert(db_url.to_string(), pool);
-    }
-    let pool = pools.get(db_url).unwrap();
-    let result = sqlx::query(sql).execute(&*pool).await.unwrap();
-    result.rows_affected() as Int
-
+    let client = pools.entry(db_url.to_string()).or_insert_with(|| {
+        Client::connect(db_url, NoTls).unwrap()
+    });
+    client.execute(sql, &[]).unwrap_or(0) as Int
 }
 
-fn reflective_get(row: &PgRow, index: usize) -> String {
-    let column_type = row.columns().get(index).map(|c| c.type_info().name()).unwrap();
+fn reflective_get(row: &postgres::Row, index: usize) -> String {
+    let column_type = row.columns().get(index).map(|c| c.type_().name()).unwrap();
     // see https://docs.rs/sqlx/0.8.2/sqlx/postgres/types/index.html
-    println!("column_type: {}", column_type);
     let value = match column_type {
-        "BOOL" => {
+        "bool" => {
             let v: Option<bool> = row.get(index);
             v.map(|v| v.to_string())
         }
-        "VARCHAR" | "CHAR(N)" | "TEXT" | "NAME" | "CITEXT" => {
+        "varchar" | "char(n)" | "text" | "name" => {
             let v: Option<String> = row.get(index);
             v
         }
-        "CHAR" => {
+        "char" => {
             let v: i8 = row.get(index);
             Some(String::from((v as u8) as char))
         }
-        "INT2" | "SMALLSERIAL" | "SMALLINT" => {
+        "int2" | "smallserial" | "smallint" => {
             let v: Option<i16> = row.get(index);
             v.map(|v| v.to_string())
         }
-        "INT" | "INT4" | "SERIAL" => {
+        "int" | "int4" | "serial" => {
             let v: Option<i32> = row.get(index);
             v.map(|v| v.to_string())
         }
-        "INT8" | "BIGSERIAL" | "BIGINT" => {
+        "int8" | "bigserial" | "bigint" => {
             let v: Option<i64> = row.get(index);
             v.map(|v| v.to_string())
         }
-        "FLOAT4" | "REAL" => {
+        "float4" | "real" => {
             let v: Option<f32> = row.get(index);
             v.map(|v| v.to_string())
         }
-        "FLOAT8" | "DOUBLE PRECISION" => {
+        "float8" | "double precision" => {
             let v: Option<f64> = row.get(index);
             v.map(|v| v.to_string())
         }
-        "DATE" => {
+        "date" => {
             let v: Option<time::Date> = row.get(index);
             v.map(|v| v.to_string())
         }
-        "TIME" => {
+        "time" => {
             let v: Option<time::Time> = row.get(index);
             v.map(|v| v.to_string())
         }
-        "TIMESTAMP" | "TIMESTAMPTZ" => {
+        "timestamp" | "timestamptz" => {
             // with-chrono feature is needed for this
             let v: Option<chrono::DateTime<Utc>> = row.get(index);
             v.map(|v| v.to_string())
         }
-        "UUID" => {
+        "uuid" => {
             let v: Option<Uuid> = row.get(index);
             v.map(|v| v.to_string())
         }
@@ -118,11 +105,11 @@ mod tests {
     #[test]
     fn test_spike() {}
 
-    #[tokio::test]
-    async fn test_query() {
+    #[test]
+    fn test_query() {
         let sql = "SELECT name FROM city";
-        let db_url = "postgres://postgres:postgres@localhost/demo";
-        let rows = pg_query(db_url, sql).await;
+        let db_url = "postgresql://postgres:postgres@localhost/demo";
+        let rows = pg_query(db_url, sql);
         for key in rows.to_vec() {
             let value = rows.get(&key);
             println!("{}: {}", key, value.to_string());
