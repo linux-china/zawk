2285db88b03386d3e392f80cabbf7513990f472f
Libing Chen <libing.chen@gmail.com>
2024-09-15 18:10:04 +0800

    chore: introduce StrMap

--- src/runtime/config_util.rs
+++ src/runtime/config_util.rs
@@ -3,16 +3,24 @@ use std::fs::File;
 use std::io::BufReader;
 use anyhow::Result;
 use ini::configparser::ini::Ini;
+use crate::runtime::{Str, StrMap};
 
-
-pub fn load_config(file: &str) -> Result<HashMap<String, String>> {
+pub fn read_config(file: &str) -> StrMap<Str> {
+    let mut map = hashbrown::HashMap::new();
     if file.ends_with(".ini") {
-        read_ini(file)
+        if let Ok(data_map) = read_ini(file) {
+            for (key, value) in data_map {
+                map.insert(Str::from(key), Str::from(value));
+            }
+        }
     } else if file.ends_with(".properties") {
-        read_properties(file)
-    } else {
-        Err(anyhow::anyhow!("Unsupported file format"))
+        if let Ok(data_map) = read_properties(file) {
+            for (key, value) in data_map {
+                map.insert(Str::from(key), Str::from(value));
+            }
+        }
     }
+    StrMap::from(map)
 }
 
 fn read_ini(ini_file: &str) -> Result<HashMap<String, String>> {
