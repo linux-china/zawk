6d85dc799a99bb6147ebf2785b19dd42cf543b5c
Libing Chen <libing.chen@gmail.com>
2025-03-30 17:50:34 +0800

    chore: use new api

--- src/runtime/s3.rs
+++ src/runtime/s3.rs
@@ -1,12 +1,12 @@
-use std::io::Write;
-use minio::s3::args::{UploadObjectArgs};
+use minio::s3::builders::ObjectContent;
 use minio::s3::client::Client;
 use minio::s3::creds::StaticProvider;
 use minio::s3::error::Error;
 use minio::s3::http::BaseUrl;
-use minio::s3::response::UploadObjectResponse;
+use minio::s3::response::{PutObjectContentResponse};
 use minio::s3::types::S3Api;
-use minio::s3::utils::Multimap;
+use std::io::Write;
+use std::path::Path;
 use tempfile::NamedTempFile;
 
 fn s3_client() -> Result<Client, Error> {
@@ -16,11 +16,7 @@ fn s3_client() -> Result<Client, Error> {
     let s3_region = std::env::var("S3_REGION").unwrap();
     let mut base_url = s3_endpoint.parse::<BaseUrl>()?;
     base_url.region = s3_region;
-    let static_provider = StaticProvider::new(
-        &s3_access_key,
-        &s3_access_secret,
-        None,
-    );
+    let static_provider = StaticProvider::new(&s3_access_key, &s3_access_secret, None);
     let client = Client::new(
         base_url.clone(),
         Some(Box::new(static_provider)),
@@ -30,7 +26,10 @@ fn s3_client() -> Result<Client, Error> {
     Ok(client)
 }
 
-pub fn get_object(bucket_name: &str, object_name: &str) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+pub fn get_object(
+    bucket_name: &str,
+    object_name: &str,
+) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
     let client = s3_client().unwrap();
     let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(async {
@@ -41,7 +40,11 @@ pub fn get_object(bucket_name: &str, object_name: &str) -> Result<String, Box<dy
     })
 }
 
-pub fn put_object(bucket_name: &str, object_name: &str, body: &str) -> Result<UploadObjectResponse, Box<dyn std::error::Error + Send + Sync>> {
+pub fn put_object(
+    bucket_name: &str,
+    object_name: &str,
+    body: &str,
+) -> Result<PutObjectContentResponse, Box<dyn std::error::Error + Send + Sync>> {
     let client = s3_client().unwrap();
     let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(async {
@@ -49,17 +52,15 @@ pub fn put_object(bucket_name: &str, object_name: &str, body: &str) -> Result<Up
         let _ = file.write_all(body.as_bytes());
         let file_path = file.path().to_str().unwrap().to_string();
         // upload args
-        let mut upload_object_args = UploadObjectArgs::new(bucket_name, object_name, &file_path).unwrap();
-        let content_type = mime_guess::from_path(object_name).first_or_octet_stream().to_string();
-        upload_object_args.content_type = &content_type;
-        let mut headers: Multimap = Multimap::new();
-        headers.insert("x-amz-acl".to_string(), "public-read".to_string());
-        upload_object_args.headers = Some(&headers);
-        let response = client.upload_object(&upload_object_args).await?;
+        let content = ObjectContent::from(Path::new(file_path.as_str()));
+        let response = client
+            .put_object_content(bucket_name, object_name, content)
+            .send()
+            .await
+            .unwrap();
         Ok(response)
     })
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -72,7 +73,7 @@ mod tests {
     #[test]
     fn test_s3_get() {
         dotenv::dotenv().ok();
-        let text = get_object( BUCKET, OBJECT_NAME).unwrap();
+        let text = get_object(BUCKET, OBJECT_NAME).unwrap();
         assert_eq!(text, BODY);
     }
 
